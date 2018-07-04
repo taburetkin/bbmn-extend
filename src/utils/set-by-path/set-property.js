d59f5001d1a686efb8b0b72a748c60c5270deeb1
admin <admin@nrg.plus>
2018-07-04 17:21:37 +0300

    fix get-property import path

--- src/utils/set-by-path/set-property.js
+++ src/utils/set-by-path/set-property.js
@@ -1,4 +1,4 @@
-import getProperty from './get-property';
+import getProperty from '../get-by-path/get-property';
 import isModel from '../../bb/is-model';
 function setProperty(context, name, value, options) {
 	if (isModel(context)) {
