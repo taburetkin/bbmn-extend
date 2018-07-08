7010c3d6bad8915fc863a80823cc24020d6f8566
dimaver <dima.taburetkin@gmail.com>
2018-07-08 12:57:56 +0300

    doc build

--- gulp/docs.js
+++ gulp/docs.js
@@ -1,5 +1,5 @@
 import gulp from 'gulp';
-import concat from 'gulp-concat';
+import concat from 'gulp-concat-util';
 import del from 'del';
 import _ from 'underscore';
 import gulpSequence from 'gulp-sequence';
@@ -11,6 +11,7 @@ const foldersTasks = _(folders).map(getFolderTask);
 function clean()
 {
 	let readmes = _(folders).map(folder => `src/${folder}/README.md`);
+	readmes.push('src/README.md');
 	return del(readmes);
 }
 gulp.task('clean:docs', clean);
@@ -23,11 +24,21 @@ _(folders).each(folder => {
 function buildFolder(folder){
 	return gulp.src(`src/${folder}/**/README.md`)
 		.pipe(concat('README.md'))
+		.pipe(concat.header(`# ${folder} \r\n`))
 		.pipe(gulp.dest(`src/${folder}`));
 }
-// function buildFolders()
-// {
-// 	var folderTasks = _(folders).map(folder => buildFolder(folder));
-// }
 
-gulp.task('docs', gulpSequence('clean:docs', foldersTasks));
+gulp.task('concat:docs', function(){
+	let paths = _(folders).map(folder => `src/${folder}/README.md`);
+	return gulp.src(paths)
+		.pipe(concat('README.md'))	
+		.pipe(gulp.dest('src/'));
+});
+
+gulp.task('compile:docs',() => {
+	return gulp.src(['./header.md', 'src/README.md'])
+		.pipe(concat('README.md'))	
+		.pipe(gulp.dest('./'));
+});
+
+gulp.task('docs', gulpSequence('clean:docs', foldersTasks, 'concat:docs', 'compile:docs'));
