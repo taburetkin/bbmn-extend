09c2269d8590d0ed392c88d486c62209b1e06822
admin <admin@nrg.plus>
2018-07-18 16:08:11 +0300

    camel-case first letter toLowerCase

--- src/utils/camel-case/index.js
+++ src/utils/camel-case/index.js
@@ -3,5 +3,8 @@
 export default function camelCase(text, first) {
 	if (!_.isString(text)) return text;
 	var splitter = first === true ? /(^|:)(\w)/gi : /(:)(\w)/gi;
-	return text.replace(splitter, (match, prefix, text) => text.toUpperCase());
+	text = text.replace(splitter, (match, prefix, text) => text.toUpperCase());
+	if(!first)
+		text = text.replace(/(^)(\w)/gi, (match, prefix, text) => text.toLowerCase());
+	return text;
 }
