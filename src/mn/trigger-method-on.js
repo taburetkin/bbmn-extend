23ec399a5243ebc5842ba47661e20cf4a57e22d1
dimaver <dima.taburetkin@gmail.com>
2018-07-22 14:47:00 +0300

    mn v4 does not have triggerMethodOn, so i have take it

--- src/mn/trigger-method-on.js
+++ src/mn/trigger-method-on.js
@@ -1,2 +1,11 @@
-import Mn from 'backbone.marionette';
-export default Mn.triggerMethodOn;
+import { triggerMethod } from 'backbone.marionette';
+
+export default function triggerMethodOn(context, ...args) {
+	if (_.isFunction(context.triggerMethod)) {
+		return context.triggerMethod.apply(context, args);
+	}
+
+	return triggerMethod.apply(context, args);
+}
+
+//export default Mn.triggerMethodOn;
