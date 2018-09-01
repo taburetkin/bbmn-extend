39387e059535bfc21c74668d078f7835ecc378c1
dimaver <dima.taburetkin@gmail.com>
2018-09-01 16:40:11 +0300

    mixins/next-collection-view

--- src/mixins/collection-view/index.js
+++ src/mixins/collection-view/index.js
@@ -1,6 +1,6 @@
 import emptyFetch from './empty-fetch';
 import improvedIndexes from './improved-indexes';
-import nextCollectionView from './improved-indexes';
+import nextCollectionView from './next-collection-view';
 export {
 	emptyFetch,
 	improvedIndexes,
