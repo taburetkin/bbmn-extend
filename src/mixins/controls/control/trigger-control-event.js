038b0f20c8ea5d1f0d4448f7d16fe732a4d14354
dimaver <dima.taburetkin@gmail.com>
2018-09-05 11:35:54 +0300

    mixins/control

--- src/mixins/controls/control/trigger-control-event.js
+++ src/mixins/controls/control/trigger-control-event.js
@@ -3,13 +3,19 @@ export default function triggerControlEvent(control, event, ...args){
 	//let control = _control.getParentControl() || _control;
 
 	let name = control.getControlName();
-	let eventName = name + ':' + event;
+	let eventName = 'control:' + event;
+	let namedEventName = name + ':' + event;
+
 	let method = _.isFunction(control.triggerMethod) ? control.triggerMethod
 		: _.isFunction(control.trigger) ? control.trigger
 			: function(){};
-	method.call(control, eventName, ...args);
-	
 
-	control.proxyControlEventToParent(eventName, ...args);
+	method.call(control, eventName, ...args);
+	if (eventName != namedEventName) {
+		method.call(control, namedEventName, ...args);
+	}
+
+
+	control.proxyControlEventToParent(namedEventName, ...args);
 
 }
