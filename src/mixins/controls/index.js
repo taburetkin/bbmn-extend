1e8d744619a1891cb4572b0697fd4771f7db9b43
dimaver <dima.taburetkin@gmail.com>
2018-09-11 11:53:59 +0300

    mixins/controls fix export

--- src/mixins/controls/index.js
+++ src/mixins/controls/index.js
@@ -1,5 +1,8 @@
-import control from './control';
-import input from './input';
+import button from './button/index.js';
+import control from './control/index.js';
+import controlView from './control-view/index.js';
+import input from './input/index.js';
+import promiseBar from './promise-bar/index.js';
 export {
-	control, input
+	button, control, controlView, input, promiseBar
 };
