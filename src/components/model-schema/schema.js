f1600564b5929246912b9e6bb5df4f065c731ef0
dimaver <dima.taburetkin@gmail.com>
2018-09-20 17:39:58 +0300

    base-model

--- src/components/model-schema/schema.js
+++ src/components/model-schema/schema.js
@@ -1,5 +1,5 @@
-import mix from '../../utils/mix/index.js';
+import BaseModel from '../base-model/index.js';
 import getOption from '../../mixins/common/get-option/index.js';
 
-const Base = mix(function Schema(){}).with(getOption);
-export default Base;
+const Schema = getOption(BaseModel);
+export default Schema;
--- src/utils/is-known-ctor/ctors.js
+++ src/utils/is-known-ctor/ctors.js
@@ -1,13 +1,14 @@
 import { Model, Collection, BackboneView as View, Router } from '../../vendors/backbone.js';
 import { MnObject } from '../../vendors/marionette.js';
 import Mn from 'backbone.marionette';
-
+import BaseModel from '../../components/base-model/index.js';
 let ctors = [
 	Model,
 	Collection,
 	View,
 	Router,
 	MnObject,
+	BaseModel
 ];
 
 let tryGetFromMn = ['Region', 'Application', 'AppRouter'];
